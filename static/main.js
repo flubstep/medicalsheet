2a9e1095066d79909c458d420e3721e7d5cde0f8
Albert Sheu <albert.sheu@gmail.com>
2017-05-20 11:07:29 -0700

    Some style changes for mouse cursor

--- static/main.js
+++ static/main.js
@@ -1,8 +1,11 @@
 window.addEventListener('load', function() {
 
   var imageOverlay = document.getElementById('image-overlay');
+  var container = document.getElementById('container');
+
   imageOverlay.addEventListener('click', function() {
     imageOverlay.className = 'leaving';
+    container.className = '';
     setTimeout(function() {
       imageOverlay.className = '';
       imageOverlay.innerHTML = '';
@@ -14,6 +17,7 @@ window.addEventListener('load', function() {
       e.stopPropagation();
       var clicked = e.target;
       var newImage = clicked.cloneNode();
+      container.className = 'noscroll';
       imageOverlay.appendChild(newImage);
       imageOverlay.className = 'active';
     }).bind(img));
